910653b188a43922dd7e044dc5aaa079bac6ac84
Joakim G <joagre@gmail.com>
2020-11-26 02:34:25 +0100

    working on random walk based on perlin noise in order to test the habitat eliposes. One day more.

--- src/habitat.erl
+++ src/habitat.erl
@@ -9,12 +9,7 @@
          f2 :: number(),
          r :: number()}).
 
-t1() ->
-    
-
-
-
-test() ->
+test1() ->
     Latitude = 51.509865,
     Longitude = -0.118092,
     {X, Y, _Z} = geodetic_to_ecef_coordinates(Latitude, Longitude, 0),
@@ -26,6 +21,63 @@ test() ->
     Alpha = calculate_alpha(W, T),
     Beta = 60,
     {H, update_habitat(H, NewL, Alpha, Beta)}.
+
+test2() ->
+    epx:start(),
+    Width = 2048,
+    Height = 2048,
+    Pixels = epx:pixmap_create(Width, Height, argb),
+    epx:pixmap_attach(Pixels),
+    Background = epx:pixmap_create(Width, Height, argb),
+    epx:pixmap_fill(Background, {255, 255, 255, 255}),
+    Window = epx:window_create(
+               0, 0, Width, Height, [button_press, button_release]),
+    epx:window_attach(Window),
+    Plot = fun(X, Y) ->
+                   SafeX = window_wrap(X, Width),
+                   SafeY = window_wrap(Y, Height),
+                   %%io:format("3: ~p\n", [{X, Y}]),
+                   epx:draw_ellipse(Background, SafeX, SafeY, 2, 2),
+                   %%epx:pixmap_put_pixel(Background, SafeX, SafeY, black),
+                   update_window(Width, Height, Window, Pixels, Background),
+                   {SafeX, SafeY}
+           end,
+    iterate(Window,
+            fun() -> noise(0.05) end,
+            fun() -> noise(0.05) end,
+            Width / 2, Height / 2, Plot).
+
+window_wrap(Value, Limit) when Value > Limit -> 0;
+window_wrap(Value, Limit) when Value < 0 -> Limit;
+window_wrap(Value, _Limit) -> Value.
+
+iterate(Window, NextXDelta, NextYDelta, X, Y, Plot) ->
+    {XDelta, EvenNextXDelta} = NextXDelta(),
+    {YDelta, EvenNextYDelta} = NextYDelta(),
+    NewX = X + (XDelta * 8 - 4),
+    NewY = Y + (YDelta * 8 - 4),
+    {SafeX, SafeY} = Plot(NewX , NewY),
+    case is_window_closed(Window, 0) of
+        true ->
+            ok;
+        false ->
+            %%timer:sleep(100),
+            iterate(Window, EvenNextXDelta, EvenNextYDelta, SafeX, SafeY, Plot)
+    end.
+
+is_window_closed(Window, Timeout) ->
+    receive
+        {epx_event, Window, close} ->
+            epx:window_detach(Window),
+            true
+    after
+        Timeout ->
+            false
+    end.
+
+update_window(Width, Height, Window, Pixels, Background) ->
+    epx:pixmap_copy_to(Background, Pixels),
+    epx:pixmap_draw(Pixels, Window, 0, 0, 0, 0, Width, Height).
 
 %% Section 3.3.1
 init_habitat(L0) ->
@@ -45,12 +97,12 @@ update_focal_points(#habitat{f1 = F1, f2 = F2, r = 0}, L, Alpha, Beta) ->
             _ ->
                 {F2, F1}
         end,
-    {new_f1(L, Alpha, Beta, F1Old), new_f2(L, Alpha, Beta, F2Old)}.
+    {new_f1(L, Alpha, F1Old), new_f2(L, Alpha, Beta, F2Old)}.
 
 d({X1, Y1}, {X2, Y2}) ->
     math:sqrt(math:pow(X2 - X1, 2) + math:pow(Y2 - Y1, 2)).
 
-new_f1({X, Y}, Alpha, Beta, {XOld, YOld}) ->
+new_f1({X, Y}, Alpha, {XOld, YOld}) ->
     {X * Alpha + XOld * (1 - Alpha), Y * Alpha + YOld * (1 - Alpha)}.
 
 new_f2({X, Y}, Alpha, Beta, {XOld, YOld}) ->
@@ -66,6 +118,34 @@ update_radius(OldR, L, Alpha, F1, F2) ->
 calculate_alpha(W, T) ->
     2 / (T * W + 1).
 
+%%
+%% Perlin noise
+%%
+
+%% https://en.wikipedia.org/wiki/Perlin_noise
+noise(Step) ->
+    A = rand:uniform(),
+    B = rand:uniform(),
+    {A, fun() -> noise(Step, Step, A, B) end}.
+
+noise(1, Step, _A, B) ->
+    {B, fun() -> noise(Step, Step, B, rand:uniform()) end};
+noise(Travel, Step, _A, B) when Travel > 1 ->
+    NextB = rand:uniform(),
+    InterpolatedB = smoothstep(B, NextB, 1 - Travel),
+    {InterpolatedB, fun() -> noise(Step - (1 - Travel), Step, B, NextB) end};
+noise(Travel, Step, A, B) ->
+    InterpolatedB = smoothstep(A, B, Travel),
+    {InterpolatedB, fun() -> noise(Travel + Step, Step, A, B) end}.
+
+%% https://en.wikipedia.org/wiki/Smoothstep
+smoothstep(A, B, W) ->
+    (B - A) * (3.0 - W * 2.0) * W * W + A.
+
+%%
+%% Conversion from geodetic to ecef coordinates
+%%
+
 %% https://en.m.wikipedia.org/wiki/Geographic_coordinate_conversion#From_geodetic_to_ECEF_coordinates 
 %% https://en.m.wikipedia.org/wiki/Geodetic_datum#World_Geodetic_System_1984_(WGS_84)
 geodetic_to_ecef_coordinates(Latitude, Longitude, H) ->
@@ -80,7 +160,8 @@ geodetic_to_ecef_coordinates(Latitude, Longitude, H) ->
     B = 6356752.3142,
     B2 = math:pow(B, 2),
     %% Prime vertical radius of curvature
-    N = A2 / math:sqrt(math:pow(CLatitude, 2) * A2 + math:pow(SLatitude, 2) * B2),
+    N = A2 / math:sqrt(
+               math:pow(CLatitude, 2) * A2 + math:pow(SLatitude, 2) * B2),
     X = (N + H) * CLatitude * CLongitude, 
     Y = (N + H) * CLatitude * SLongitude, 
     Z  = (B2 / A2 * N + H) * SLatitude,
